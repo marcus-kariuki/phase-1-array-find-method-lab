2bf34a61611c123eed2c1eb0b630e7f71fcdcf12
Marcus <marcus.kariuki@student.moringaschool.com>
2022-12-06 08:49:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  function superbowlWin(stats){
+    const yearWithWin = stats.find((record) => record.result ==='W');
+    return yearWithWin!== undefined?yearWithWin.year:undefined;
+  }
